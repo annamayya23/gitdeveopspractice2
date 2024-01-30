90c55152711cddc74224897bc29d3e4204f1babb
annamayya23 <kesannagariannamayya@gmail.com>
2024-01-30 15:56:36 +0530

    Access modifiers

---
+++ Modifiers.java
@@ -0,0 +1,12 @@
+What are an Access modifier and non-access modifier in java?
+-> Access modifiers:- public, private, default and protected.
+-> Non-access modifiers:- final, static, abstract, synchronized, transient, volatile and strictfp
+
+Types of access modifiers like private, default, protected and public.
+-> private:- The access level of a private modifier is with in the class. It cannot be accessed from outside the class.
+-> default:- A java default keyword is an access modifier.default access modifier is access with in the class and outside the class. If you didn't assign any access modifier to variables, methods, constructors and classes, by default it is considered as default access modifier.
+-> protected:- protected is keyword in java, it can access with in class, outside the class and within the packages.
+-> public:- public is a keyword in java, public can access within the project. 
+Types of non-access modifiers like abstract, final, native, static, synchronized modifier.
+
+
